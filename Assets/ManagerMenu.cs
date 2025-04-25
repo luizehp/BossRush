f7c21274df2200011ca59c90e6122e569588a667
Luca Caruso <lucac@al.insper.edu.br>
2025-04-25 11:58:58 -0300

    MainMenu quase pronto

---
+++ Assets/ManagerMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ManagerMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void playGame()
+    {
+        Debug.Log("Play Game button clicked!");
+        // Add your game starting logic here
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
