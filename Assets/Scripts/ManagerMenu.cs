1e40100cd1d3587bc63f841e13301b5a47f72acd
Luca Caruso <lucac@al.insper.edu.br>
2025-05-07 18:51:55 -0300

    Menu Inicial Pronto

--- Assets/Scripts/ManagerMenu.cs
+++ Assets/Scripts/ManagerMenu.cs
@@ -1,24 +1,73 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
-using System.Collections;
+using UnityEngine.UI;
 
 public class ManagerMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Popup de Controles")]
+    public GameObject controlsPopup;
+
+    [Header("Popup de Configurações")]
+    public GameObject settingsPopup;
+
+    [Header("Configurações de Cena")]
+    public string startSceneName = "GameScene";
+
+    [Header("Configurações")]
+    public Slider volumeSlider;
+
     void Start()
     {
-        
+        if (controlsPopup != null)
+            controlsPopup.SetActive(false);
+
+        if (settingsPopup != null)
+            settingsPopup.SetActive(false);
+
+        if (volumeSlider != null)
+            volumeSlider.value = AudioListener.volume;
     }
 
-    public void playGame()
+    // chamado pelo botão “Iniciar”
+    public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(2);
+        if (!string.IsNullOrEmpty(startSceneName))
+            SceneManager.LoadSceneAsync(startSceneName);
+        else
+            Debug.LogError("ManagerMenu: defina startSceneName no Inspector!");
     }
-    
-    // Update is called once per frame
-    void Update()
+
+    // chamado pelo botão “Controles”
+    public void ShowControls()
     {
-        
+        if (controlsPopup != null)
+            controlsPopup.SetActive(true);
+    }
+
+    // chamado pelo “X” no popup de controles
+    public void HideControls()
+    {
+        if (controlsPopup != null)
+            controlsPopup.SetActive(false);
+    }
+
+    // chamado pelo botão “Configurações”
+    public void ShowSettings()
+    {
+        if (settingsPopup != null)
+            settingsPopup.SetActive(true);
+    }
+
+    // chamado pelo “Fechar” no popup de configurações
+    public void HideSettings()
+    {
+        if (settingsPopup != null)
+            settingsPopup.SetActive(false);
+    }
+
+    // vinculado ao Slider → On Value Changed
+    public void SetVolume(float vol)
+    {
+        AudioListener.volume = vol;
     }
 }
