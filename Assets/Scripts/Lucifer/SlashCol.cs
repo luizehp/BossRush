b0f394ac31f177201762b9cf08bd0ce177fb1a9b
luizehp <luizehp@al.insper.edu.br>
2025-05-16 10:45:11 -0300

    pre

--- Assets/Scripts/Lucifer/SlashCol.cs
+++ Assets/Scripts/Lucifer/SlashCol.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 
 public class SlashCol : MonoBehaviour
 {
-    public bool slashCol = false;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) slashCol = true;
+        GameManager.Instance.hasSlashAbility = true;
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/SlashPre.cs
+++ Assets/Scripts/SlashPre.cs
@@ -5,14 +5,10 @@ public class Slash : MonoBehaviour
     public GameObject slashPrefab;
     public float extendedRange = 1f;
     public Transform playerTransform;
-    public SlashCol slashCol;
 
     public void SlashPre(Vector2 direction)
     {
-        if (!slashCol.slashCol)
-        {
-            return;
-        }
+        if (!GameManager.Instance.hasSlashAbility) return;
         Vector2 spawnPosition = (Vector2)transform.position + direction * extendedRange;
 
         GameObject slash = Instantiate(slashPrefab, spawnPosition, Quaternion.identity);
