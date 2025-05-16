7d58ccdea2f219b2cd2602b7f16c54db3171b3ab
Joao-antonio-gg <joaoagg@al.insper.edu.br>
2025-05-16 08:33:31 -0300

    Agr o Shader funciona

---
+++ Assets/Scripts/Beholder/DeathSetup.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DeathSetup : MonoBehaviour
+{
+    public GameObject portaPrefab;
+
+    void Awake()
+    {
+        Animator animator = GetComponent<Animator>();
+        foreach (var behaviour in animator.GetBehaviours<Death>())
+        {
+            behaviour.portaPrefab = portaPrefab;
+        }
+    }
+}
