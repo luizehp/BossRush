2c315520a525a611aabcee042b7f40583a01fc8b
luizehp <luizehp@al.insper.edu.br>
2025-05-16 11:24:26 -0300

    a

--- Assets/Scripts/Lucifer/Flame.cs
+++ Assets/Scripts/Lucifer/Flame.cs
@@ -5,10 +5,15 @@ public class Flame : MonoBehaviour
     public ParticleSystem flame1;
     public ParticleSystem flame2;
 
+    public GameObject light1;
+    public GameObject light2;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            light1.SetActive(true);
+            light2.SetActive(true);
             var emission1 = flame1.emission;
             emission1.enabled = true;
             var emission2 = flame2.emission;
